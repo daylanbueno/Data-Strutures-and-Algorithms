b55a2f04a0b7382d8cc9cd7b273815ee85df8115
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2022-01-23 20:15:35 -0300

    implemented my soluction for sorted value

---
+++ secao 4 - Data Structure Arrays/MegerSortedArray.js
@@ -0,0 +1,22 @@
+
+// check parameter
+// merge  array
+// sorted array 
+
+function mergeSortedArrayMySoluction(arr1, arr2) {
+    const sortedArray = [...arr1, ...arr2]
+    for (let i = 0;  i < sortedArray.length; i++) {
+        for (let j = i + 1; j < sortedArray.length; j++) {
+            if (sortedArray[j] < sortedArray[i]) {
+                const valueItemJ = sortedArray[j] 
+                sortedArray[j]  = sortedArray[i] 
+                sortedArray[i] = valueItemJ
+            }
+        }
+    }
+    return sortedArray
+} // N(N^2) no so good soluction.
+
+
+console.log(mergeSortedArrayMySoluction([56,3,0,31],[4,6,30]))
+
